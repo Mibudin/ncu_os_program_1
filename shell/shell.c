2b9d68dd3c787be5c51d157063302eeae62374b4
Mibudin <jj9028j9028@gmail.com>
2021-03-26 01:18:02 +0800

    Try shell.

--- shell/shell.c
+++ shell/shell.c
@@ -1,7 +1,48 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+
+#define TERMINAL_PROMPT "$ "
+
+#define MAX_COMMMAND_LENGTH 256
+
+#define EXIT_SUCCESS 0
+#define EXIT_ERROR   1
+
+
+void shell_init();
+int  shell_loop();
 
 
 int main()
 {
-    
+    shell_init();
+
+    shell_loop();
+
+    return EXIT_SUCCESS;
+}
+
+void shell_init()
+{
+
+}
+
+void shell_loop()
+{
+    char cmd[256];
+    char args[128];
+
+    while(true)
+    {
+        // READ
+        // PARSE
+        // EXCUTE: fork inside
+    }
+
+    return;
 }
